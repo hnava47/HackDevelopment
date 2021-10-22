913f7c6d6ff008f4dbf645093b8231db70233bdb
Hector Nava <hector.r.nava44@gmail.com>
2021-10-21 20:56:53 -0600

    Added namespace and updated file name

--- helloHack.php
+++ helloHack.php
@@ -1,6 +1,8 @@
 <?hh
 // Running Hack on PHP file
 
+namespace Hack\UserDocumentation\Fundamentals\ProgramStructure\Examples\LegacyHelloWorld;
+
 <<__EntryPoint>>
 function main(): void {
   echo "Hello World!\n";
